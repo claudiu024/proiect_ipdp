356a83507fa62a76fdbd501a52e12fc70d289f41
andreeahaldan <100340435+andreeahaldan@users.noreply.github.com>
2022-03-08 15:08:10 +0200

    Add files via upload

--- restaurant-react-app/src/App.js
+++ restaurant-react-app/src/App.js
@@ -3,12 +3,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-   
-    
-      </header>
+    <body>
+    <div className="login-div">
+        <h1>RESTAURANT</h1>
+        <form action="/action_page.php">
+        <div className="user-div">
+      
+        <input className="text-input"type="text" id="user" name="user" placeholder="E-mail adress"></input>
+  
+        <input className="text-input" type="password" id="pwd" name="pwd" placeholder="Password"></input>
+        </div>
+        <input className="connect-input"type="submit" value="Connect" ></input>
+
+        </form>
+      <div className='link-div'>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ai uitat parola?
+        </a>
+        </div>
+
+        <button className='connect-input'>Create new account</button>
+
     </div>
+    </body>
   );
 }
 
