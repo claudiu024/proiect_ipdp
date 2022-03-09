b58509144dc30930a0e6b09c770bf6b97f78bf3f
claudiu024 <100803383+claudiu024@users.noreply.github.com>
2022-03-09 19:22:32 +0200

    Add files via upload

--- restaurant-react-app/src/App.js
+++ restaurant-react-app/src/App.js
@@ -1,12 +1,18 @@
-
 import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
-    <body>
+   
+    // <body>
+  
     <div className="login-div">
         <h1>RESTAURANT</h1>
-        <form action="/action_page.php">
+        <form action="">
         <div className="user-div">
       
         <input className="text-input"type="text" id="user" name="user" placeholder="E-mail adress"></input>
@@ -19,19 +25,45 @@ function App() {
       <div className='link-div'>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href=""
+       
           rel="noopener noreferrer"
         >
-          Ai uitat parola?
+        Forgot password?
         </a>
         </div>
-
-        <button className='connect-input'>Create new account</button>
-
+          <div className="line"></div>
+        <button  href="register.html" className='connect-input register-button '> <Link to="/register" className="App-link">Create new account</Link></button>
+       
     </div>
-    </body>
+  //  </body>
   );
 }
 
+
+function Register() {
+return(
+  // <body>
+      <div class="login-div">
+          <h1 >Register</h1>
+          <form action="register.js">
+          <div className="user-div" style="margin: 0;">
+          <input className="text-input input-register" type="text" id="user" name="user" placeholder="Nume"></input>
+          <input className="text-input input-register"type="text" id="user" name="user" placeholder="Prenume"></input>
+    
+    
+          <input className="text-input input-register"type="text" id="user" name="user" placeholder="E-mail adress"></input>
+    
+          <input className="text-input input-register" type="password" id="pwd" name="pwd" placeholder="Password"></input>
+          <input className="text-input input-register" type="password" id="pwd" name="pwd" placeholder="Confirm Password"></input>
+          </div>
+          <input className="connect-input connect-input-register" type="submit" value="Create account" ></input>
+          </form>
+      </div>
+  // </body>
+)
+
+  
+
+}
 export default App;
