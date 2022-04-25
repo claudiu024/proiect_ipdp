29b5560dab0327fd4895d929285cc0a0661ce687
Claudiu Andronache <100803383+claudiu024@users.noreply.github.com>
2022-04-25 09:26:23 +0300

    Add files via upload

--- client/src/login.js
+++ client/src/login.js
@@ -1,20 +1,53 @@
-import { Link } from "react-router-dom"
+import { Link ,useHistory} from "react-router-dom"
 import { Menu } from "./components/menu"
+import Axios from "axios";
+import { useState } from 'react';
 import Login_container from "./components/login_container"
+
+
+
 export default function Login(){
+  const [Email,setEmail]=useState("");
+  const [Password,setPassword]=useState("");
+
+const history=useHistory();
+const [component_list,setComponent]=useState();
+ function add_component(){ setComponent(<span className="field_error">Email or Password doesn't match</span>)}
+  const submit =()=>{
+
+    Axios.post('http://localhost:3001/verify-login',{
+            Email:Email,
+            Password:Password
+        }).then(()=>{
+            console.log("Verified")
+            // console.log(res)
+            history.push('/')
+        }).catch(()=>{
+          console.log("User or password doesn't match ")
+          add_component()
+        
+        });};
     return(
       <div>
     
       <Login_container title="Login"  >
  
-       
-        <input className="text-input"type="text"  placeholder="E-mail adress"></input>
+       <div>
+        <input className="text-input"type="text"  placeholder="E-mail adress"
+         onChange={(event)=>setEmail(event.target.value)}/>
+
+        <input className="text-input" type="password" placeholder="Password"
+        onChange={(event)=>setPassword(event.target.value)}/>
+        </div>
+          {component_list} 
+        {/* <Link to="/"> */}
+          <button className="connect-input"type="submit" 
+         onClick={submit}
+        >
+           Connect</button>
+           {/* </Link> */}
   
-        <input className="text-input" type="password" placeholder="Password"></input>
-        
-        <Link to="/"><button className="connect-input"type="submit">Connect</button></Link>
-  
-      <div className='link-div'>
+      <div className='link-div'>  
         <Link  to="/passwordreset" className="App-link" >Forgot password? </Link>
         </div>
     
--- client/src/register.js
+++ client/src/register.js
@@ -3,20 +3,29 @@ import './App.css';
 import { Menu} from "./components/menu" ;
 import Axios from "axios";
 import Login_container from './components/login_container';
-import { Link } from 'react-router-dom';
-
-
+import { useHistory } from 'react-router-dom';
+// import PhoneInput from 'react-phone-number-input'
+import 'react-phone-input-2/lib/style.css'
+import PhoneInput from 'react-phone-input-2'
 function Register(){
+    const history=useHistory();
+    const [component_list,setComponent]=useState();
     const [first_name, setfirst_name]=useState("");
     const [last_name,setlast_name]=useState("");
     const [Phone_number,setPhone_number]=useState("");
     const [Adress,setAdress]=useState("");
     const [Email,setEmail]=useState("");
     const [Password,setPassword]=useState("");
+    const [ConfirmPassword,setConfirmPassword]=useState("");
 
-    const submit =()=>{
-
-Axios.post('http://localhost:4060/create',{
+  
+    function add_component(){ setComponent(<span className="field_error">Password doesn't match</span>)}
+    const submit =(e)=>{  
+         e.preventDefault();  //previne refresh-ul paginii
+        
+if(Password==ConfirmPassword ){
+  
+Axios.post('http://localhost:3001/create',{
         first_name:first_name,
         last_name:last_name,
         Phone_number:Phone_number,
@@ -24,39 +33,62 @@ Axios.post('http://localhost:4060/create',{
         Email:Email,
         Password:Password
     }).then(()=>{
+        history.push("/login")
         console.log("successefull insert")
-    });};
+       
+    })}
+    
+    else {
+      
+        add_component();
+        console.log("password doesn't match")
+    }
+  
+    };
     return(
     
         <div>
         {/* <Menu/> */}
         <Login_container title="Register">
-        
-              {/* <form action="login"> */}
+        <form onSubmit={submit}>
+    
               <div className="user-div">
-              <input className="text-input small-text" type="text" placeholder="First Name" 
+              <input className="text-input small-text" type="text" placeholder="First Name" required
                onChange={(event)=>setfirst_name(event.target.value)} />
            
-              <input className="text-input small-text"type="text" placeholder="Last Name"
+              <input className="text-input small-text"type="text" placeholder="Last Name" required 
               onChange={(event)=>setlast_name(event.target.value)}/>
 
-              <input className="text-input larger-text" type="text" placeholder="Phone number"
-              onChange={(event)=>setPhone_number(event.target.value)}/>
+              <PhoneInput value={Phone_number} placeholder="Phone number" 
+              required  
+                country="ro" countryCodeEditable={false} enableSearch disableSearchIcon
+                // containerStyle={{textAlign:'center'}}
+                // dropdownClass={'x'}
+               
+                // containerClass="text-input larger-text"
+                buttonClass="x"
+                inputClass={'text-input larger-text'}
+                // dropdownContainerClass="x"
+              onChange={setPhone_number }/>
 
-              <input className="text-input larger-text"type="text" placeholder="Adress(city, street, number, postal code)"
+              <input className="text-input larger-text"type="text" placeholder="Adress(city, street, number, postal code)" 
               onChange={(event)=>setAdress(event.target.value)}/>
 
-              <input className="text-input larger-text"type="text" placeholder="E-mail adress"
+              <input className="text-input larger-text" type="email" placeholder="E-mail adress" required
               onChange={(event)=>setEmail(event.target.value)}/>
 
-              <input className="text-input small-text" type="password" placeholder="Password"
+              <input className="text-input small-text" type="password" placeholder="Password" required
               onChange={(event)=>setPassword(event.target.value)}/>
               
-              <input className="text-input small-text" type="password" placeholder="Confirm Password"/>
+              <input className="text-input small-text" type="password" placeholder="Confirm Password" required
+              onChange={(event)=>setConfirmPassword(event.target.value)}/>
               </div>
-             <Link  to= "/login"><button onClick={submit} className="connect-input connect-input-register" type="submit" value="Create account" >Create account</button>
-             </Link> 
-              {/* </form> */}
+              {component_list} 
+             <input  className="connect-input connect-input-register" type="submit" value="Create account" >
+                 {/* Create account */}
+                 </input>
+             </form>
+          
              
               </Login_container>
             
